f32eb4b72f810e65e3ee456c5ff59c22f86fb057
Lucas <lucas.o@live.com>
2016-07-12 17:58:02 -0300

    prog

--- blocks/models.py
+++ blocks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Block(models.Model):
@@ -13,6 +15,9 @@ class Block(models.Model):
     books = models.ManyToManyField("Book", blank=True)
     websites = models.ManyToManyField("Website", blank=True)
     courses = models.ManyToManyField("Course", blank=True)
+
+    #user that owns this block
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
