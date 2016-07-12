0e79bf2896860fd7dc7dac1addd6425f8b8f1d0b
Lucas <lucas.o@live.com>
2016-07-12 18:01:47 -0300

    Created register module for register new users

---
+++ register/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return redirect('subjects_owner_display')
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {
+        'form': form,
+    })
