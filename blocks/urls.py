0932bedb9cefc8a4a1440d68ce4bdfff73b7439f
Lucas <lucas.o@live.com>
2016-07-15 09:28:20 -0300

    Block app replaced by Subject
    Block app replaced by Subject due to 'block' name conflict with django
    framework

--- blocks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.blocksDisplay, name='blocks_index'),
-    url(r'^add/', views.blockAdd, name='blocks_add'),
-    url(r'^details/', views.blockDetails, name='block_details'),
-]
