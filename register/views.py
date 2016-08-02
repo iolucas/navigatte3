4318aa3b0d72ead4144e5df4c109f873dff39936
Lucas <lucas.o@live.com>
2016-08-02 14:07:29 -0300

    Added register disabled feature

--- register/views.py
+++ register/views.py
@@ -2,10 +2,15 @@
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 
+from navigatte.settings import REGISTER_DISABLED
+
 def register(request):
+    if REGISTER_DISABLED:
+        return HttpResponse("Register temporary disabled.")
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
